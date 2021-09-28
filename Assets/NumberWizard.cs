20e9a1a5ddb48580939ec4d93d52ed1cb27ba9f5
mtrimner <matt.trimner@gmail.com>
2021-09-28 16:27:11 -0500

    first commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcometo number wizard");
+        Debug.Log("Please enter a number");
+        Debug.Log("Highest Number: 1000");
+        Debug.Log("Lowest Number: 1");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
