cfa968370d08edb46b7a25933e1cdf5005910fd9
mtrimner <matt.trimner@gmail.com>
2021-10-01 16:40:44 -0500

    added inputs

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
+
         Debug.Log("Welcometo number wizard");
         Debug.Log("Please enter a number");
-        Debug.Log("Highest Number: 1000");
-        Debug.Log("Lowest Number: 1");
+        Debug.Log("Highest Number: " + max);
+        Debug.Log("Lowest Number: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = correct");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Return key was pressed.");
+        }
     }
 }
